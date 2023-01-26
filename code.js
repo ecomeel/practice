02598edad981f75e15fbf4f163ee4954182d3aba
alex.pulin <ecomeel@gmail.com>
2023-01-26 23:14:40 +0300

    26.1 end

--- code.js
+++ code.js
@@ -1,14 +1,1 @@
-// numbersArray = [10, 324, 1, 43, 543, 64, 4, 323, 44];
-
-// function compareNums(num1, num2) {
-// 	if (num1 > num2) {
-// 		return 1
-// 	} else if (num1 === num2) {
-// 		return 0
-// 	} else {
-// 		return -1
-// 	}
-// }
-
-// numbersArray.sort(compareNums);
-// console.log(numbersArray)
+let a = 5;
