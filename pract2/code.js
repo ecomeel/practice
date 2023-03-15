776a9e09dbf774c679bfdf0557f280560e078eb4
ecomeel <ecomeel@gmail.com>
2023-03-15 20:34:33 +0300

    reset

--- pract2/code.js
+++
@@ -1,5 +0,0 @@
-const element = document.querySelector('.lesson__item-list_red');
-const elementStyle = getComputedStyle(element)
-console.log(elementStyle.paddingLeft);
-const paddingLeft = parseInt(elementStyle.paddingLeft);
-console.log(paddingLeft);
